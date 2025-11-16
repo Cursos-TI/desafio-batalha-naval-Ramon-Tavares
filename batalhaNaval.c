ed4f4211b03657699b26e843381ea1845ebc9358
Ramon-Tavares <ramontavares500@gmail.com>
2025-11-16 13:21:44 +0000

    nivel mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -16,23 +16,69 @@ int main() {
 
 
     // barco na horizontal
-    for (int i = 3; i <= 5; i++){
-        tabuleiro[2][i] = 3;
+    for (int i = 6; i <= 8; i++){
+        tabuleiro[3][i] = 3;
     }
 
     // barco vertical
-    for (int j = 5; j <= 7; j++){
-        tabuleiro[j][7] = 3;
+    for (int j = 0; j <= 2; j++){
+        tabuleiro[j][5] = 3;
     }
 
 
     // linhas diagonal
-    for (int i = 6, j = 1; i <=8; i++, j++){
+    for (int i = 0, j = 7; i <=2; i++, j++){
         tabuleiro[i][j] = 3;
     }
 
     for (int i = 0, j = 2; i <=2; i++, j--){
         tabuleiro[i][j] = 3;
+    }
+
+    // cone
+    for (int i = 7; i <= 9; i++){
+        for (int j = 0; j < 10; j++){
+            if (j >= 9 - i){
+                if (j <= i - 5){
+                    tabuleiro[i][j] = 5;
+                }       
+            }     
+        } 
+    }
+
+
+    // octaedro
+    for (int i = 3; i <= 5; i++){
+        for (int j = 0; j < 10; j++){
+            if (j == 2){
+                tabuleiro[i][j] = 5;
+            }
+            if (i == 4){
+                if (j == 1){
+                    tabuleiro[i][j] = 5;
+                }
+                if (j == 3) {
+                    tabuleiro[i][j] = 5;
+                } 
+            }    
+        }   
+    }
+
+
+    // cruz
+    for (int i = 5; i <= 9; i++){
+        for (int j = 0; j < 10; j++){
+            if (j == 7){
+                tabuleiro[i][j] = 5;
+            }
+            if (i == 7){
+                if (j >= 5){
+                    if (j <= 9){
+                        tabuleiro[i][j] = 5;
+                    }  
+                }  
+            }
+        }  
     }
     
     
@@ -56,33 +102,7 @@ int main() {
         }
         printf("\n");
     }
-
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
 
     return 0;
 }
